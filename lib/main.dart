9dd79ab2b1d02c684fce5625225c97fadbafc2df
Zeck Lowe <lowezeck@gmail.com>
2023-11-01 17:52:38 +0800

    last commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:news_app/assets/app_theme.dart';
 import 'package:news_app/screens/home_screen.dart';
 import 'package:news_app/screens/navigation_bar.dart';
-import 'package:news_app/screens/news_page_screen.dart';
-import 'package:news_app/screens/profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
